6a858a4ec4245e5b8c6b8ac2bda2590fdfecefbf
kclee04 <143151932+kclee04@users.noreply.github.com>
2023-12-13 21:29:46 -0800

    Add files via upload

---
+++ rooms.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+if [ "$#" -eq 1 ]; then
+  starting_location="$1"
+else
+  starting_location="$(pwd)"
+fi
+
+cd $starting_location
+mkdir startroom
+cd startroom
+echo "A cold room" > desc.txt
+
+# second room
+mkdir east
+cd east
+echo "A dark room" > desc.txt
+
+# third room
+mkdir east
+mkdir west
+cd west
+touch goback.txt
+cd ..
+cd east
+touch desc.txt
+echo "A scary room" > desc.txt
+
+# fourth room
+mkdir north
+mkdir south
+mkdir west
+cd west
+touch goback.txt
+cd .. 
+cd south # item room
+echo "Found sword" > item.txt
+mkdir north
+cd north
+touch goback.txt
+cd ..
+cd ..
+cd north
+touch desc.txt
+echo "A bright room" > desc.txt
+
+# sixth room
+mkdir north
+mkdir south
+cd south
+touch goback.txt
+cd ..
+cd north
+touch desc.txt
+echo "A warm room" > desc.txt
+
+#seventh room
+mkdir east
+mkdir west
+mkdir south
+cd south
+touch goback.txt
+cd ..
+cd west #eighth room
+echo "A windy room" > desc.txt
+mkdir east
+cd east
+touch goback.txt
+cd .. # seventh
+cd ..
+cd east
+touch desc.txt
+echo "A crowded room" > desc.txt
+
+# ninth room
+mkdir east
+mkdir west
+cd west
+touch goback.txt
+cd ..
+cd east
+touch desc.txt
+echo "A small room" > desc.txt
+
+# connector room
+mkdir west
+cd west
+touch goback.txt
+cd ..
+mkdir east
+cd east
+echo "A damp room" > desc.txt
+# run next person bash script in here
+mkdir west
+cd west
+touch goback.txt
+cd ..
+mkdir north
+cd north
